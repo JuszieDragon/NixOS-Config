fd048cca7d69777655bfc54e4a9066ce34e0b6ea
JustinHJJohnson <justin.h.j.johnson@gmail.com>
2024-04-09 17:50:45 +1000

    update zsh aliases

--- modules/home-manager/zsh.nix
+++ modules/home-manager/zsh.nix
@@ -17,8 +17,9 @@
       cpk = "cat /etc/current-system-packages";
       nixosbuild = "sudo nixos-rebuild switch --flake ~/nixos#default";
       fup = "nix flake update";
-      webbuildandload = "web-ext build -n a2o4.xpi && firefox-devedition web-ext-artifacts/a2o4.xpi";
+      webbuildandload = "web-ext build -n a2o4.xpi --overwrite-dest && firefox-developer-edition web-ext-artifacts/a2o4.xpi";
       img = "qimgv .";
+      setvol = "amixer -c S9 set 'PCM',1 69";
     };
 
     oh-my-zsh = {
