129cfaece92f8e703554db71de815736133afc53
Juszie Dragon <justin.h.j.johnson@gmail.com>
2025-12-21 21:32:40 +1100

    setup mangadex gallery-dl config

--- modules/home-manager/gallery-dl.nix
+++ modules/home-manager/gallery-dl.nix
@@ -2,8 +2,32 @@
   programs.gallery-dl = {
     enable = true;
     settings = {
-      #TODO make mangafox download to volume folders
-      extractor.base-directory = "/mnt/media/gallery-dl";
+      extractor = {
+        archive = "/state/gallery-dl/archive.sqlite3";
+        base-directory = "/mnt/media/gallery-dl";
+        skip = "abort:10";
+        
+        #TODO could use this to make cbzs
+        #https://github.com/mikf/gallery-dl/issues/7471
+        mangadex = {
+          filename = {
+            "subcategory == 'covers'" = "{volume:>03}_{lang}.{extension}";
+            "" = "p{page:>03}.{extension}";
+          };
+          directory = {
+            "subcategory == 'covers'" = ["{category}" "{manga}" "covers"];
+            volume = ["{category}" "{manga}" "v{volume}" "c{chapter:>03}_{title}"];
+            "" = ["{category}" "{manga}" "c{chapter:>03}"];
+          };
+          postprocessors = [
+            {
+              name = "metadata";
+              event = "init";
+              filename = "info.json";
+            }
+          ];
+        };
+      };
     };
   };
 }
