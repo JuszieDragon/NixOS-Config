02657cc488525e9a46b07fc179ce493679aea2eb
Juszie Dragon <justin.h.j.johnson@gmail.com>
2026-04-19 13:13:45 +1000

    update niri outputs

--- modules/home-manager/niri/outputs.nix
+++ modules/home-manager/niri/outputs.nix
@@ -2,15 +2,16 @@
   programs.niri.settings.outputs = {
     #Kogan
     DP-1 = {
-      position = { x = 200; y = 1080; };
+      position = { x = 0; y = 1080; };
     };
     #Philips
     DP-2 = {
-      position = { x = 0; y = 0; };
+      position = { x = 3440; y = 600; };
+      transform.rotation = 270;
     };
     #Viewsonic
     DP-3 = {
-      position = { x = 1920; y = 0; };
+      position = { x = 800; y = 0; };
     };
     #LG C4
     HDMI-A-1.enable = false;
