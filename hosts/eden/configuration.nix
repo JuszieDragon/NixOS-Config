004f6e3c2e3bad2e273cf995bededd13516a0355
Juszie Dragon <justin.h.j.johnson@gmail.com>
2026-08-11 19:33:06 +1000

    add desktop config

--- hosts/eden/configuration.nix
+++ hosts/eden/configuration.nix
@@ -1,10 +1,4 @@
-# Edit this configuration file to define what should be installed on
-# your system. Help is available in the configuration.nix(5) man page, on
-# https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
-
-{ config, inputs, lib, pkgs, ... }:
-
-{
+{ config, inputs, lib, pkgs, ... }: {
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
@@ -12,124 +6,111 @@
       ../default.nix
     ];
 
-  # Use the systemd-boot EFI boot loader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = false;
-
-  hardware.asahi = {
-    enable = true;
-    peripheralFirmwareDirectory = inputs.self + /firmware;
+  boot.loader = {
+    systemd-boot.enable = true;
+    efi.canTouchEfiVariables = false;
   };
 
-  networking.hostName = "eden";
-
-  # Configure network connections interactively with nmcli or nmtui.
-  networking.networkmanager = {
-    enable = true;
-    wifi.backend = "iwd";
+  hardware = {
+    asahi = {
+      enable = true;
+      peripheralFirmwareDirectory = inputs.self + /firmware;
+    };
+    bluetooth.enable = true;
+    graphics = {
+      enable = true;
+      enable32Bit = true;
+    };
   };
 
-  # Set your time zone.
-  time.timeZone = "Australia/Hobart";
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Select internationalisation properties.
-  # i18n.defaultLocale = "en_US.UTF-8";
-  # console = {
-  #   font = "Lat2-Terminus16";
-  #   keyMap = "us";
-  #   useXkbConfig = true; # use xkb.options in tty.
-  # };
-
-  # Enable the X11 windowing system.
-  # services.xserver.enable = true;
-
-
-  
-
-  # Configure keymap in X11
-  # services.xserver.xkb.layout = "us";
-  # services.xserver.xkb.options = "eurosign:e,caps:escape";
-
-  # Enable CUPS to print documents.
-  # services.printing.enable = true;
-
-  # Enable sound.
-  # services.pulseaudio.enable = true;
-  # OR
-  services.pipewire = {
-    enable = true;
-    pulse.enable = true;
+  networking = {
+    hostName = "eden";
+    firewall.enable = false;
+    networkmanager = {
+      enable = true;
+      wifi.backend = "iwd";
+    };
   };
 
-  # Enable touchpad support (enabled default in most desktopManager).
-  services.libinput.enable = true;
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.justin = {
-    isNormalUser = true;
-    extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
-    packages = with pkgs; [
+  environment = {
+    # https://discourse.nixos.org/t/hyprland-dolphin-file-manager-trying-to-open-an-image-asks-for-a-program-to-use-for-open-it/69824/3
+    etc."xdg/menus/applications.menu".source =
+      "${pkgs.kdePackages.plasma-workspace}/etc/xdg/menus/plasma-applications.menu";
+    systemPackages = with pkgs; [
+      faugus-launcher
+      foliate
       git
-      tree
+      gnome-disk-utility
+      hydrus
+      localsend
+      usbutils
+      mpv
       neovim
+      orca-slicer
+      pulseaudio
+      qimgv
+      restic
+      restic-browser
+      simple-scan
+      swaybg
+      swaylock
+      unrar
+      wget
+      wiremix
+      vesktop
+      xwayland-satellite
+
+      kdePackages.ark
+      kdePackages.dolphin
+      kdePackages.dolphin-plugins
+      kdePackages.qtsvg
+      kdePackages.baloo-widgets
+      kdePackages.baloo
+      kdePackages.kio
+      kdePackages.kio-extras
+      kdePackages.kservice
+
+      libreoffice-qt
+      hunspell
+      hunspellDicts.en_AU-large
+
+      inputs.noctalia.packages.${pkgs.stdenv.hostPlatform.system}.default
     ];
   };
 
-  # programs.firefox.enable = true;
+  xdg.portal = {
+    enable = true;
+    wlr.enable = false;
 
-  # List packages installed in system profile.
-  # You can use https://search.nixos.org/ to find more packages (and options).
-  environment.systemPackages = with pkgs; [
-  #   vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-    wget
-  ];
+    extraPortals = [
+      pkgs.xdg-desktop-portal-gnome
+      pkgs.xdg-desktop-portal-gtk
+    ];
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
+    config = {
+      common = {
+        default = [ "gnome" "gtk" ];
+      };
+      niri = {
+        default = [ "gnome" "gtk" ];
+        "org.freedesktop.impl.portal.FileChooser" = "gtk";
 
-  # List services that you want to enable:
+        #fix discord screensharing with niri
+        "org.freedesktop.portal.ScreenCast" = "gnome";
+        "org.freedesktop.portal.Screenshot" = "gnome";
+      };
+    };
+  };
 
-  # Enable the OpenSSH daemon.
-  services.openssh.enable = true;
+  services = {
+    openssh.enable = true;
+    pipewire = {
+      enable = true;
+      pulse.enable = true;
+    };
+    slibinput.enable = true;
+  };
 
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  networking.firewall.enable = false;
-
-  # Copy the NixOS configuration file and link it from the resulting system
-  # (/run/current-system/configuration.nix). This is useful in case you
-  # accidentally delete configuration.nix.
-  # system.copySystemConfiguration = true;
-
-  # This option defines the first version of NixOS you have installed on this particular machine,
-  # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
-  #
-  # Most users should NEVER change this value after the initial install, for any reason,
-  # even if you've upgraded your system to a new NixOS release.
-  #
-  # This value does NOT affect the Nixpkgs version your packages and OS are pulled from,
-  # so changing it will NOT upgrade your system - see https://nixos.org/manual/nixos/stable/#sec-upgrading for how
-  # to actually do that.
-  #
-  # This value being lower than the current NixOS release does NOT mean your system is
-  # out of date, out of support, or vulnerable.
-  #
-  # Do NOT change this value unless you have manually inspected all the changes it would make to your configuration,
-  # and migrated your data accordingly.
-  #
-  # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
   system.stateVersion = "26.11"; # Did you read the comment?
-
 }
 
